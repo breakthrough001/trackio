5f1de702552044642c08f0ee3910f125c845afe5
Joe Naples <jnaples90@gmail.com>
2022-03-09 08:02:59 -0500

    Capture user savings goal from form

--- app.js
+++ app.js
@@ -1,52 +1,67 @@
 // Selectors
 const progressBar = document.querySelector('.progress-done');
 const progressBarPercentage = document.querySelector('.progressBarPercentage');
+const barProgress = document.querySelector('.bar-progress');
+const progressBarGoal = document.querySelector('.progress-bar-goal');
 
 const userInput = document.querySelector('.user-input');
+const userGoal = document.querySelector('.user-goal');
 const inputList = document.querySelector('.input-list');
 const submitButton = document.querySelector('button');
 let inputValues = [];
 
 // Select Data Attribute
 let value = parseInt(progressBarPercentage.innerHTML.slice(0, -1));
+// Progress Bar Default State
+progressBar.style.opacity = 0;
 
 // Event Listeners
 submitButton.addEventListener('click', addUserInput);
 
-// Progress Bar Default State
-progressBar.style.opacity = 0;
-
+// Functions
 // Add User Input to List
 function addUserInput(event) {
   event.preventDefault();
   const newLI = document.createElement('li');
   const newUserInput = `$${userInput.value}`;
+  // Capture User Goal Input
+  const newUserGoal = parseInt(userGoal.value);
   newLI.innerText = newUserInput;
   inputList.appendChild(newLI);
 
   // Turn user input into a number
   value = parseInt(userInput.value);
-
   // Push and Store input values in an array
   inputValues.push(value);
   // Sum up all numbers in the array
   const sumOfInputs = inputValues.reduce(
     (prevVal, currVal) => prevVal + currVal
   );
+  progressBarStyles(sumOfInputs, newUserGoal);
+  clearUserInputs();
+}
+
+function clearUserInputs() {
+  userInput.value = '';
+  userGoal.value = '';
+}
+
+function progressBarStyles(sumOfInputs, newUserGoal) {
   // Set width of progress bar to user inputs
   progressBar.style.width = sumOfInputs + '%';
   // Set padding left on percent number above progress bar
   progressBarPercentage.style.paddingLeft = sumOfInputs - 4 + '%';
-  progressBarPercentage.innerHTML = sumOfInputs + '%';
+  // Set User Goal
+  progressBarPercentage.innerHTML = '$' + sumOfInputs;
+  progressBarGoal.innerHTML = '$' + newUserGoal;
+  // Show Progress Bar
   progressBar.style.opacity = 1;
-  // Sum array values up
-  // Reset input value
-  userInput.value = '';
+  // Inner Bar Percent
+  barProgress.innerHTML = (sumOfInputs / newUserGoal) * 100 + '%';
+  console.log(sumOfInputs);
+  console.log(newUserGoal);
 }
 
-// progress bar is gonna be an array, each time user adds a number it is pushed to end of array and then summed up -- if they delete one it is sliced or whatever them summed up again
 // if no value entered make error message pop up Enter Value
 
-// create empty array
-// push user input input into that array and total it to equal the width of
-// the progress bar
+// need to capture user goal outside submit function and store it as a number
