5c214f905358f1373b1144f3d9d7ae61fc7f6242
Joe Naples <jnaples90@gmail.com>
2022-03-09 13:55:48 -0500

    Finished logic for progress bar

--- app.js
+++ app.js
@@ -5,9 +5,11 @@ const barProgress = document.querySelector('.bar-progress');
 const progressBarGoal = document.querySelector('.progress-bar-goal');
 
 const userInput = document.querySelector('.user-input');
-const userGoal = document.querySelector('.user-goal');
+const userWish = document.querySelector('#user-wish');
 const inputList = document.querySelector('.input-list');
-const submitButton = document.querySelector('button');
+const submitButton = document.querySelector('.input-button');
+const goalButton = document.querySelector('.goal-button');
+const goalFormInput = document.querySelector('.goal-form');
 let inputValues = [];
 
 // Select Data Attribute
@@ -17,6 +19,7 @@ progressBar.style.opacity = 0;
 
 // Event Listeners
 submitButton.addEventListener('click', addUserInput);
+goalButton.addEventListener('click', captureUserGoal);
 
 // Functions
 // Add User Input to List
@@ -25,7 +28,7 @@ function addUserInput(event) {
   const newLI = document.createElement('li');
   const newUserInput = `$${userInput.value}`;
   // Capture User Goal Input
-  const newUserGoal = parseInt(userGoal.value);
+  const newUserGoal = global;
   newLI.innerText = newUserInput;
   inputList.appendChild(newLI);
 
@@ -43,25 +46,43 @@ function addUserInput(event) {
 
 function clearUserInputs() {
   userInput.value = '';
-  userGoal.value = '';
 }
 
 function progressBarStyles(sumOfInputs, newUserGoal) {
-  // Set width of progress bar to user inputs
-  progressBar.style.width = sumOfInputs + '%';
-  // Set padding left on percent number above progress bar
-  progressBarPercentage.style.paddingLeft = sumOfInputs - 4 + '%';
-  // Set User Goal
-  progressBarPercentage.innerHTML = '$' + sumOfInputs;
-  progressBarGoal.innerHTML = '$' + newUserGoal;
-  // Show Progress Bar
-  progressBar.style.opacity = 1;
-  // Inner Bar Percent
-  barProgress.innerHTML = (sumOfInputs / newUserGoal) * 100 + '%';
-  console.log(sumOfInputs);
-  console.log(newUserGoal);
+  const roundedNumber = Math.round((sumOfInputs / newUserGoal) * 100);
+  if (sumOfInputs >= global) {
+    // Show Progress Bar
+    progressBar.style.opacity = 1;
+    // set bar to 100%
+    progressBar.style.width = 100 + '%';
+    // Hide Progress Currency
+    progressBarPercentage.innerHTML = '';
+    // Inner Bar Percent
+    barProgress.innerHTML = 100 + '%';
+  } else {
+    // Show Progress Bar
+    progressBar.style.opacity = 1;
+    // Set width of progress bar to user inputs
+    progressBar.style.width = roundedNumber + '%';
+    // Set padding left on percent number above progress bar
+    progressBarPercentage.style.paddingLeft = roundedNumber - 4 + '%';
+    // Set User Goal
+    progressBarPercentage.innerHTML = '$' + sumOfInputs;
+    // Inner Bar Percent
+    barProgress.innerHTML = roundedNumber + '%';
+  }
+}
+
+let global;
+
+function captureUserGoal(e) {
+  e.preventDefault();
+  global = parseInt(userWish.value);
+  // Number at end of progress bar
+  progressBarGoal.innerHTML = '$' + global;
+  global.value = '';
 }
 
 // if no value entered make error message pop up Enter Value
-
 // need to capture user goal outside submit function and store it as a number
+// function needs to return a variable so it can be used outside of the function
