c4318e7d28cc75ff10d7d97511510f52efd4d179
Joe Naples <jnaples90@gmail.com>
2022-03-09 06:57:26 -0500

    Fixed logic for progress bar and userinput is now working

--- app.js
+++ app.js
@@ -1,60 +1,40 @@
 // Selectors
-const progressBar = document.querySelector(".progress-done");
-const progressBarPercentage = document.querySelector(".progressBarPercentage");
+const progressBar = document.querySelector('.progress-done');
+const progressBarPercentage = document.querySelector('.progressBarPercentage');
 
-const userInput = document.querySelector(".user-input");
-const inputList = document.querySelector(".input-list");
-const submitButton = document.querySelector("button");
+const userInput = document.querySelector('.user-input');
+const inputList = document.querySelector('.input-list');
+const submitButton = document.querySelector('button');
+
+progressBar.style.opacity = 0;
 
 // Select Data Attribute
-const dataContainer = document.querySelector("#data-container");
-// Get value in data-value
-let dataValueHolder = parseInt(dataContainer.getAttribute("data-value"));
-console.log(dataValueHolder);
-
-const user = document.querySelector("[data-value]");
+let value = parseInt(progressBarPercentage.innerHTML.slice(0, -1));
 
 // Event Listeners
-submitButton.addEventListener("click", addUserInput);
-
-// Functions
+submitButton.addEventListener('click', addUserInput);
 
 // Add User Input to List
 function addUserInput(event) {
-   event.preventDefault();
-   const newLI = document.createElement("li");
-   const newUserInput = `$${userInput.value}`;
-   newLI.innerText = newUserInput;
-   inputList.appendChild(newLI);
+  event.preventDefault();
 
-   // Change data-value
+  const newLI = document.createElement('li');
+  const newUserInput = `$${userInput.value}`;
+  newLI.innerText = newUserInput;
+  inputList.appendChild(newLI);
+
+  // Change data-value
+  value = parseInt(userInput.value);
+  progressBarPercentage.innerHTML = value + '%';
+
+  // Set width of progress bar to user input
+  progressBar.style.width = value + '%';
+  // Set padding left on percent number above progress bar
+  progressBarPercentage.style.paddingLeft = value - 4 + '%';
+  progressBar.style.opacity = 1;
+  // Reset input value
+  userInput.value = '';
 }
-
-// Progress Bar Styling
-progressBar.style.opacity = 1;
-// This line is getting the value of data-value as a number then adding a % to it to set the width of the bar to that width
-progressBar.style.width = progressBar.getAttribute("data-value") + "%";
-// Set padding left on percent number above progress bar
-progressBarPercentage.style.paddingLeft =
-   progressBar.getAttribute("data-value") - 4 + "%";
-
-// console.log(progressBar.getAttribute('data-value'));
-// console.log(progressBarPercentage.innerHTML.slice(0, -1));
-//
-//
-//
-//
-//
-//
-// submitButton.addEventListener('click', () => {
-//   // num has to equal the input value number user enters
-//   let num = userInput.value;
-//   // let num = progressData.setAttribute('data-value', num);
-//   console.log('data-value', num);
-//   progressData
-//     .setAttribute(num) // progressData.style.width = progress.getAttribute('data-value') + '%';
-//     .document.querySelector('.percentage').innerHTML = `${num}%`;
-// });
 
 // progress bar is gonna be an array, each time user adds a number it is pushed to end of array and then summed up -- if they delete one it is sliced or whatever them summed up again
 // if no value entered make error message pop up Enter Value
